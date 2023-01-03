5be3adb5b744dda176b4b88ff40f4d0f91a0cf80
Klaus Rehnberg <klaus.rehnberg@aalto.fi>
2023-01-03 16:08:29 +0200

    Simple GUI done

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import random
+import os
+import pygame
+
+
+def find_files(directory):
+    files_found = []
+    for path, subdirs, files in os.walk(directory):
+        for name in files:
+            files_found.append(os.path.join(path, name))
+    return files_found
+
+def find_file(file_name, directory):
+    file_found = ""
+    for path, subdirs, files in os.walk(directory):
+        for name in files:
+            if(file_name == name):
+                file_found = os.path.join(path,name)
+
+    return file_found
 
 
 class Cell:
@@ -6,8 +25,11 @@ class Cell:
         self.x = x
         self.y = y
         self.grid = board.grid
-        self.options = [[0, 0, 0,0],[1,1,1,1], [1,1,0,0], [0,0,1,1]]
+        self.options = []
         self.collapsed = False
+
+        for file in find_files("Tiles"):
+            self.options.append(file[6:-4])
 
     def neighbors(self):
         if self.x > 0:
@@ -25,25 +47,32 @@ class Cell:
                 yield (self.grid[self.x + 1][self.y - 1], 2)
             if self.y < len(self.grid[0])-1:
                 yield (self.grid[self.x + 1][self.y + 1], 4)
-    
+
     def choose_random_option(self):
         result = random.choice(self.options)
 
-        for option in self.options:
-            if option != result:
-                self.options.remove(option)
-        
+        index = 0
+        while index < len(self.options):
+            if self.options[index] != result:
+                self.options.remove(self.options[index])
+            else:
+                index += 1
+
         self.collapsed = True
 
+        for neighbor in self.neighbors():
+            neighbor[0].collapse((neighbor[1]+4) %
+                                 8, self.handshake(neighbor[1]))
+
     def handshake(self, direction):
-        
+
         if direction % 2 == 0:
-            all_possibilities = []  
+            all_possibilities = []
             for option in self.options:
                 if option[direction//2] not in all_possibilities:
                     all_possibilities.append(option[direction//2])
             return all_possibilities
-            
+
         else:
             all_possibilities2 = []
             for option in self.options:
@@ -51,39 +80,28 @@ class Cell:
                 if not (((direction-1)//2)+1) > 3:
                     second = option[((direction-1)//2)+1]
                 if (second, option[(direction-1)//2]) not in all_possibilities2:
-                    all_possibilities2.append((second, option[(direction-1)//2]))
+                    all_possibilities2.append(
+                        (second, option[(direction-1)//2]))
             return all_possibilities2
-        
-        
-
-
 
     def collapse(self, direction, value):
-        
-
 
         if not self.collapsed:
             didNothing = True
             if direction % 2 == 1:
-                
-
-
 
                 index = 0
                 while index < len(self.options):
-                    
+
                     second2 = self.options[index][0]
                     if ((direction-1)//2)+1 != 4:
                         second2 = self.options[index][((direction-1)//2)+1]
                     if (self.options[index][((direction-1)//2)], second2) not in value:
-                        
+
                         didNothing = False
                         self.options.remove(self.options[index])
                     else:
                         index += 1
-                
-
-                    
 
                 if not didNothing:
                     if len(self.options) == 1:
@@ -92,20 +110,19 @@ class Cell:
                     for neighbor in self.neighbors():
                         print(self.options)
                         print(self.handshake(neighbor[1]))
-                        neighbor[0].collapse((neighbor[1]+4)%8, self.handshake(neighbor[1]))
-                    
+                        neighbor[0].collapse((neighbor[1]+4) %
+                                             8, self.handshake(neighbor[1]))
 
             else:
                 index = 0
                 while index < len(self.options):
-                    
+
                     if self.options[index][direction//2] not in value:
-                        
+
                         didNothing = False
                         self.options.remove(self.options[index])
                     else:
                         index += 1
-
 
                 if not didNothing:
                     if len(self.options) == 1:
@@ -114,11 +131,8 @@ class Cell:
                     for neighbor in self.neighbors():
                         print(self.options)
                         print(self.handshake(neighbor[1]))
-                        neighbor[0].collapse((neighbor[1]+4)%8, self.handshake(neighbor[1]))   
-                    
-
-
-
+                        neighbor[0].collapse((neighbor[1]+4) %
+                                             8, self.handshake(neighbor[1]))
 
 
 class Board:
@@ -126,13 +140,13 @@ class Board:
         self.grid = grid
 
     def get_lowest_entropy(self):
-        lowest = [(0, 0, len(self.grid)**2 + 1)]
+        lowest = [(0, 0, 100)]
         for i in range(len(self.grid)):
             for j in range(len(self.grid[0])):
                 entropy = len(self.grid[i][j].options)
                 if entropy == lowest[0][2]:
                     lowest.append((i, j, entropy))
-                elif entropy < lowest[0][2]:
+                elif entropy < lowest[0][2] and entropy > 1:
                     lowest.clear()
                     lowest.append((i, j, entropy))
         return random.choice(lowest)[0:2]
@@ -144,24 +158,55 @@ class Board:
                     return False
         return True
 
+    def wave_function(self):
+        while not self.is_done():
+            target = self.get_lowest_entropy()
+            self.grid[target[0]][target[1]].choose_random_option()
+
 
 def main():
-    HEIGHT = 3
-    WIDTH = 3
+    HEIGHT = 10
+    WIDTH = 10
 
-    board = Board([]) 
+    board = Board([])
     for i in range(WIDTH):
         board.grid.append([])
         for j in range(HEIGHT):
             board.grid[i].append(Cell(i, j, board))
 
-    board.grid[1][1].collapse(1, [(1,1)])
+    board.wave_function()
 
     for row in board.grid:
         for cell in row:
             print(cell.x, cell.y)
-            print(cell.options)
+            print(cell.options[0])
 
+    pygame.init()
+
+
+    X = HEIGHT*32
+    Y = WIDTH*32
+
+    screen = pygame.display.set_mode((X, Y))
+
+    pygame.display.set_caption("Map")
+
+    imps = []
+    for file_name in find_files("Tiles"):
+        imps.append(pygame.image.load(file_name).convert())
+    
+    running = True
+
+    while running:
+        for i in range(len(board.grid)):
+            for j in range(len(board.grid[0])):
+                screen.blit(pygame.image.load(find_file(f"{board.grid[i][j].options[0]}.png", "Tiles")).convert(), (i*32, j*32))
+
+
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT:
+                running = False
+        pygame.display.update()
 
 
 if __name__ == "__main__":
